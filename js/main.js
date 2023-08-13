200815c46a32255f42a534c792928bc38d0e87dc
Petrus Mahardika <petrus.mahardika@gmail.com>
2023-08-13 17:56:19 +1000

    add error message

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello from final project, axios');
+const FTG_SEARCH_URL = 'https://www.freetogame.com/api/games';
+
+const searchGameButton = document.querySelector('.search-button');
+const gameQueryText = document.querySelector('.search-bar');
+const errorMessageNode = document.querySelector('.error-message');
+
+
+searchGameButton.addEventListener('click', function(ev){
+  const newGameQuery = gameQueryText.value;
+  
+  if(newGameQuery.trim().length === 0) {
+    errorMessageNode.innerHTML = 'Please enter a game name, tag, or keyword.'
+    return;
+  }
+
+  loadSearchResults(newGameQuery);
+});
+
+// When user start typing on the input field, the previous error message disappear
+gameQueryText.addEventListener('input', function(ev){
+  if(ev.target.value.trim().length > 0){
+    errorMessageNode.innerHTML = '';
+  }
+});
