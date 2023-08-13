75be9fd397be2392866ba902dae4ae1db5dc083e
Petrus Mahardika <petrus.mahardika@gmail.com>
2023-08-14 00:14:29 +1000

    trying to get axios to work

--- js/main.js
+++ js/main.js
@@ -16,9 +16,43 @@ searchGameButton.addEventListener('click', function(ev){
   loadSearchResults(newGameQuery);
 });
 
+gameQueryText.addEventListener('keydown', function(ev) {
+  if (ev.key === 'Enter') {
+    const newGameQuery = gameQueryText.value;
+    
+    if (newGameQuery.trim().length === 0) {
+      errorMessageNode.innerHTML = 'Please enter a game name, tag, or keyword.';
+      return;
+    }
+    
+    loadSearchResults(newGameQuery);
+  }
+});
+
 // When user start typing on the input field, the previous error message disappear
 gameQueryText.addEventListener('input', function(ev){
   if(ev.target.value.trim().length > 0){
     errorMessageNode.innerHTML = '';
   }
 });
+
+// Load the search results from the input text entered by user
+const loadSearchResults = async (gameTag) => {
+  try {
+    // Make an AJAX request to get a list of games using the user input tag
+    const response = await axios.get(FTG_SEARCH_URL, {
+      params: {
+        tag: gameTag
+      }
+    });
+    
+    console.log(response.data);
+    // Generate and handle search results using response.data
+  } catch (error) {
+    console.log('Error loading search results', error);
+  }
+};
+
+// const generateSearchResults = (data) => {
+//   console.log(data);
+// };
