1e243b15254db2a831e0336d0125e2317dcdb7c9
Petrus Mahardika <petrus.mahardika@gmail.com>
2023-08-15 00:13:13 +1000

    added search first results

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-const FTG_SEARCH_URL = 'https://www.freetogame.com/api/games';
+
+const FTG_SEARCH_URL1 = 'https://www.freetogame.com/api/games';
+const FTG_SEARCH_URL2 = 'https://www.freetogame.com/api/filter';
 const HEROKU_PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 
 const searchGameButton = document.querySelector('.search-button');
 const gameQueryText = document.querySelector('.search-bar');
 const errorMessageNode = document.querySelector('.error-message');
+const searchResultsContainer = document.querySelector('.search-results');
+const resultContainer = document.querySelector('.result');
 
 
 searchGameButton.addEventListener('click', function(ev){
@@ -40,20 +44,50 @@ gameQueryText.addEventListener('input', function(ev){
 // Load the search results from the input text entered by user
 const loadSearchResults = (gameTag) => {
   // Make an AJAX request to get a list movies from the user input text
-  axios.get(`${HEROKU_PROXY_URL}${FTG_SEARCH_URL}`, {
+  axios.get(`${HEROKU_PROXY_URL}${FTG_SEARCH_URL2}`, {
     params: {
       tag: gameTag
     }
   })
   .then( res => {
     console.log(res.data);
-    //generateSearchResults(res.data);
+    generateSearchResults(res.data);
   })
   .catch( err => {
     console.log( 'Error loading search results', err );
   });
 };
 
-// const generateSearchResults = (data) => {
-//   console.log(data);
-// };
+const generateSearchResults = (games) => {
+  // Clear previous search results
+  searchResultsContainer.replaceChildren();
+
+  // Loop over each game result
+  games.forEach( game => {
+    const divTag = document.createElement('div');
+    divTag.className = 'result';
+    // Create the image element for the movie poster
+    const imgTag = document.createElement('img');
+    imgTag.className = "game-thumbnail";
+    imgTag.src = `https://www.freetogame.com/g/${game.id}/thumbnail.jpg`;
+    imgTag.alt = game.title;
+    divTag.appendChild(imgTag);
+
+    // Create the element to display game title
+    const titleTag = document.createElement('h3');
+    titleTag.innerHTML = game.title;
+    divTag.appendChild(titleTag);
+    
+    searchResultsContainer.appendChild(divTag);
+    searchResultsContainer.style.display = 'grid';
+          
+    // Add a click event listener to show movie details when clicked
+    // divTag.addEventListener('click', function() {
+    //   errorMessageNode.innerHTML = '';
+    //   searchResultsContainer.style.display = 'none';
+    //   showMovieDetails(movie.id);
+    // });
+    
+  });
+
+};
