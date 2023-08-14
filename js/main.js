24a37615ea9c908f8effe3d54fd2e595de409ec3
Petrus Mahardika <petrus.mahardika@gmail.com>
2023-08-15 01:52:09 +1000

    added browse by platform

--- js/main.js
+++ js/main.js
@@ -9,6 +9,52 @@ const errorMessageNode = document.querySelector('.error-message');
 const searchResultsContainer = document.querySelector('.search-results');
 const resultContainer = document.querySelector('.result');
 
+const windowsLink = document.querySelector('#windows');
+const browserLink = document.querySelector('#browser');
+const allLink = document.querySelector('#all');
+
+windowsLink.addEventListener('click', function(ev){
+  axios.get(`${HEROKU_PROXY_URL}${FTG_SEARCH_URL1}`, {
+    params: {
+      platform: 'pc'
+    }
+  })
+  .then(res => {
+    generateSearchResults(res.data);
+  })
+  .catch( err => {
+    console.log( 'Error loading search results', err );
+  });
+});
+
+browserLink.addEventListener('click', function(ev){
+  axios.get(`${HEROKU_PROXY_URL}${FTG_SEARCH_URL1}`, {
+    params: {
+      platform: 'browser'
+    }
+  })
+  .then(res => {
+    generateSearchResults(res.data);
+  })
+  .catch( err => {
+    console.log( 'Error loading search results', err );
+  });
+});
+
+allLink.addEventListener('click', function(ev){
+  axios.get(`${HEROKU_PROXY_URL}${FTG_SEARCH_URL1}`, {
+    params: {
+      platform: 'all'
+    }
+  })
+  .then(res => {
+    generateSearchResults(res.data);
+  })
+  .catch( err => {
+    console.log( 'Error loading search results', err );
+  });
+});
+
 
 searchGameButton.addEventListener('click', function(ev){
   const newGameQuery = gameQueryText.value;
@@ -66,7 +112,7 @@ const generateSearchResults = (games) => {
   games.forEach( game => {
     const divTag = document.createElement('div');
     divTag.className = 'result';
-    // Create the image element for the movie poster
+    // Create the image element for game thumbnail
     const imgTag = document.createElement('img');
     imgTag.className = "game-thumbnail";
     imgTag.src = `https://www.freetogame.com/g/${game.id}/thumbnail.jpg`;
@@ -77,7 +123,19 @@ const generateSearchResults = (games) => {
     const titleTag = document.createElement('h3');
     titleTag.innerHTML = game.title;
     divTag.appendChild(titleTag);
-    
+
+    // Create the element to display game platform
+    const platformTag = document.createElement('p');
+    platformTag.className = 'platform';
+    platformTag.innerHTML = 'Platform: ' + game.platform;
+    divTag.appendChild(platformTag);
+
+    // Create the element to display game platform
+    const shortDescTag = document.createElement('div');
+    shortDescTag.className = 'short-description';
+    shortDescTag.innerHTML = game.short_description;
+    divTag.appendChild(shortDescTag);
+
     searchResultsContainer.appendChild(divTag);
     searchResultsContainer.style.display = 'grid';
           
